e58f0ccdb91cb6b7d4725bf363a5c72a0665d889
CristianGonzalez07 <1995.cgonzalez.07@gmail.com>
2023-05-30 17:07:59 -0300

    handling of the location route and its controller, modified

--- app/controllers/locationController.ts
+++ app/controllers/locationController.ts
@@ -1,18 +1,9 @@
-import { Request, Response } from 'express';
 import { GetLocationQuery } from '../queries/getLocationQuery';
-import requestIp from 'request-ip';
-import axios from 'axios';
 
 export class LocationController {
-  public async getLocation(req: Request, res: Response) {
-    var ip = requestIp.getClientIp(req);
-    if (ip === "::ffff:127.0.0.1" || ip === "::1" || ip === null) {
-      const response = await axios.get('https://api.ipify.org?format=json');
-      ip = response ? response.data.ip : "";
-    }
+  public async getLocation(ip:string) {
     const query = new GetLocationQuery(ip || "");
-    const result = await query.execute();
-    res.json(result);
+    await query.execute();
   }
 }
 
